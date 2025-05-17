3898a8c6f8906fdade4d79fd6dcf3ba67d7155ca
Ziyad Ahmed Abdullah <93719269+ziyadahmed-as@users.noreply.github.com>
2025-05-17 07:11:22 +0300

    Advanced Sub query

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,45 @@
+--Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
 
+SELECT 
+    p.property_id,
+    p.title,
+    p.city,
+    p.price_per_night,
+    avg_rating.average_rating
+FROM 
+    Property p
+JOIN (
+    SELECT 
+        property_id,
+        AVG(guest_rating) AS average_rating
+    FROM 
+        Review
+    GROUP BY 
+        property_id
+    HAVING 
+        AVG(guest_rating) > 4.0
+) avg_rating ON p.property_id = avg_rating.property_id
+ORDER BY 
+    avg_rating.average_rating DESC;
+
+-- Write a correlated subquery to find users who have made more than 3 bookings.
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    COUNT(b.booking_id) AS booking_count
+FROM 
+    User u
+JOIN 
+    Booking b ON u.user_id = b.guest_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name, u.email
+HAVING 
+    COUNT(b.booking_id) > 3
+ORDER BY 
+    booking_count DESC;
